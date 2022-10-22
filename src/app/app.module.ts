d0536b7d82b9853b5247f490d7f3616e84fcbb41
Nimisha Wilson <nimishawilson123@gmail.com>
2022-10-23 01:04:13 +0530

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainBoardViewComponent } from './pages/main-board-view/main-board-view.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainBoardViewComponent
+    MainBoardViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
