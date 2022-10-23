b1816b201d00240b60bed33b0ad698ff81269288
Nimisha Wilson <nimishawilson123@gmail.com>
2022-10-23 21:50:11 +0530

    create card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainBoardViewComponent } from './pages/main-board-view/main-board-view.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainBoardViewComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
